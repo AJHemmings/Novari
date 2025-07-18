8b062ec635651241322c4b7022a0cd6e2a1cb8a7
Adam Hemmings <adamhemmingsdev@gmail.com>
2025-07-18 15:59:38 +0100

    Removed import socket.io to get past vercel deployment

--- src/app/api/socket/route.js
+++ src/app/api/socket/route.js
@@ -1,20 +1,20 @@
-import { NextResponse } from "next/server";
-import { Server } from "socket.io";
+// import { NextResponse } from "next/server";
+// import { Server } from "socket.io";
 
-export async function GET() {
-  if (!global.io) {
-    const io = new Server({
-      path: "api/socket",
-      addTrailingSlash: false,
-    });
-    io.on("connection", (socket) => {
-      console.log("Socket connected:", socket.id);
+// export async function GET() {
+//   if (!global.io) {
+//     const io = new Server({
+//       path: "api/socket",
+//       addTrailingSlash: false,
+//     });
+//     io.on("connection", (socket) => {
+//       console.log("Socket connected:", socket.id);
 
-      socket.on("message", (data) => {
-        socket.broadcast.emit("message", data);
-      });
-    });
-    global.io = io;
-  }
-  return NextResponse.json({ success: true });
-}
+//       socket.on("message", (data) => {
+//         socket.broadcast.emit("message", data);
+//       });
+//     });
+//     global.io = io;
+//   }
+//   return NextResponse.json({ success: true });
+// }
